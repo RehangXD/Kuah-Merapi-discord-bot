4f52fad4f267484539d1a30950d739bb09595deb
Mizua <raihanbaru396@gmail.com>
2026-08-06 01:29:40 +0700

    yes

--- cogs/Music.py
+++ cogs/Music.py
@@ -184,7 +184,7 @@ class MusicCog(commands.Cog):
             asyncio.create_task(channel.send(embed=embed))
         
         ffmpeg_options = {
-            "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+            "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -headers \"{header_str}\"",
             "options": "-vn",
         }
                 
